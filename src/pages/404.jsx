32f6a1add40c5d6d6b7c7ca7eb0ed3113fb5aec9
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-27 17:19:59 -0500

    update NotFoundPage to use custom Link component for navigation

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,9 @@
+import { Link } from '../components/Link'
+
 export default function NotFoundPage () {
   return (
     <>
-      <h1>404</h1>
+      <Link to='/'>Go back to home</Link>
       <p>Page not found</p>
       <img src='../../page-not-found.svg' alt='Not found illustration' />
     </>
