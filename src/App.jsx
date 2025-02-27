9593d3abfddbf0f2e4a83116a5b15ac81b560d2c
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-27 17:29:14 -0500

    implement lazy loading for Home and About pages using React Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { lazy, Suspense } from 'react'
 import { Router } from './components/Router'
+import { Route } from './components/Route'
 import HomePage from './pages/Home'
 import AboutPage from './pages/About'
 import SearchPage from './pages/Search'
 import NotFoundPage from './pages/404'
-import { Route } from './components/Route'
+
+const LazyHomePage = lazy(() => import('./pages/Home'))
+const LazyAboutPage = lazy(() => import('./pages/About'))
 
 const appRoutes = [
   { path: '/', Component: HomePage },
@@ -14,10 +18,12 @@ const appRoutes = [
 function App () {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={NotFoundPage}>
-        <Route path='/' Component={HomePage} />
-        <Route path='/about' Component={AboutPage} />
-      </Router>
+      <Suspense fallback={null}>
+        <Router routes={appRoutes} defaultComponent={NotFoundPage}>
+          <Route path='/' Component={LazyHomePage} />
+          <Route path='/about' Component={LazyAboutPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
